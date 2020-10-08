d48258aa348f8bef45e2a114c21992b3b47f2db1
caileighf <caileighfitzgerald@gmail.com>
2020-10-08 13:56:36 -0400

    added info to init gpsd script to enable kernel support for PPS line coming from the GPS

--- scripts/init-gpsd.sh
+++ scripts/init-gpsd.sh
@@ -2,3 +2,10 @@
 
 GPSPORT=/dev/ttyGPS
 sudo gpsd $GPSPORT -F /var/run/gpsd.sock && echo "gpsd client started on port -- $GPSPORT" || "gpsd client FAILED to start on port -- $GPSPORT"
+
+printf "\n\
+To enable special kernel support for Pulse Per Second input via a GPIO pin\n\
+to help synchronize the time edit the /boot/config.txt file and adding the following lines:\n\n\
+\
+# enable GPS PPS\n\
+dtoverlay=pps-gpio,gpiopin=4\n\n"
