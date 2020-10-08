b01c7dd0b0929d232424e9e27bf30627a4dd4ce7
caileighf <caileighfitzgerald@gmail.com>
2020-10-08 13:16:24 -0400

    hard coded /dev/ttyGPS rather than allowing any passed port -- forces user to setup udev rules

--- scripts/init-gpsd.sh
+++ scripts/init-gpsd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-GPSPORT=$1
+GPSPORT=/dev/ttyGPS
 sudo gpsd $GPSPORT -F /var/run/gpsd.sock && echo "gpsd client started on port -- $GPSPORT" || "gpsd client FAILED to start on port -- $GPSPORT"
