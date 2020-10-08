4abaf6f8def1830e8b9c419079e55ace43c24095
caileighf <caileighfitzgerald@gmail.com>
2020-10-08 14:22:30 -0400

    added NTP deps

--- scripts/install-dependancies.sh
+++ scripts/install-dependancies.sh
@@ -2,6 +2,6 @@
 
 sudo apt update && sudo apt upgrade
 sudo apt install tree screen network-manager htop virtualenv
-sudo apt install gpsd gpsd-clients python-gps pps-tools
+sudo apt install gpsd gpsd-clients python-gps pps-tools ntpsec-ntpdate
 sudo apt install gcc g++ make libusb-1.0 libusb-1.0-0-dev
 sudo apt install minicom
