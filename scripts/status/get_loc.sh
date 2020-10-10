410ed9909e67b43f83da55d14531abe33ecbe0f8
caileighf <caileighfitzgerald@gmail.com>
2020-10-10 15:55:04 -0400

    wip

--- scripts/status/get_loc.sh
+++ scripts/status/get_loc.sh
@@ -2,12 +2,12 @@
 
 # http://thomasloughlin.com/gpspipe-gps-client/
 
-gpsdata=$( timeout 2 gpspipe -w -n 5 | grep -m 1 lon )
+gpsdata=$( timeout 3 gpspipe -w -n 5 | grep -m 1 lon )
 
 if [ "$gpsdata" = "" ] ; then
     echo "No GPS data"
 else
     lat=$( echo "$gpsdata"  | jsawk 'return this.lat' )
     lon=$( echo "$gpsdata"  | jsawk 'return this.lon' )
-    echo "$lat, $lon - https://maps.google.com/maps?q=$lat,+$lon"
+    echo -e "$lat, $lon - https://maps.google.com/maps?q=$lat,+$lon"
 fi
