40e37260b2cef8c8cfcb0f4fccc90291164aa547
caileighf <caileighfitzgerald@gmail.com>
2020-10-11 00:47:04 -0400

    wip

--- scripts/utils/auto_ssh_screen.sh
+++ scripts/utils/auto_ssh_screen.sh
@@ -5,19 +5,5 @@
 
 # Only run if we are not already inside a running screen and only if in an SSH session.
 if [[ -z "${STY}" && ! -z "${SSH_CLIENT}" ]]; then
-  screen -S ACBOX -rd || screen -S ACBOX
-  # detached_screens=($(screen -ls | grep pts | grep -v Attached))
-
-  # for screen in "${detached_screens[@]}"; do
-  #   if [[ "${screen}" == *".pts"* ]]; then
-  #     IFS='.pts' read -ra split <<< "${screen}"
-  #     for id in "${split[@]}"; do
-  #       first_id="${id}"
-  #       break
-  #     done 
-  #     break
-  #   fi
-  # done
-
-  screen -R $first_id
+  screen -S ACBOX -rd || screen -S ACBOX bash -c 'help welcome; exec sh'
 fi
