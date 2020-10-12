03d39eb9a91e0c6caa7e4dc95eebe44eece74b94
caileighf <caileighfitzgerald@gmail.com>
2020-10-11 22:38:39 -0400

    wip

--- scripts/utils/auto_ssh_screen.sh
+++ scripts/utils/auto_ssh_screen.sh
@@ -5,5 +5,5 @@
 
 # Only run if we are not already inside a running screen and only if in an SSH session.
 if [[ -z "${STY}" && ! -z "${SSH_CLIENT}" ]]; then
-  screen -S ACBOX -rd || screen -S ACBOX bash -c 'help --welcome;'
+  screen -S ACBOX -rd || screen -S ACBOX -p 0 -X stuff "help --welcome^M"
 fi
