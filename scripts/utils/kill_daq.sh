7fd0393396911ddf32b556e7155f7104483ceaed
caileighf <caileighfitzgerald@gmail.com>
2020-10-13 03:06:55 -0400

    wip

--- scripts/utils/kill_daq.sh
+++ scripts/utils/kill_daq.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 PID=$(ps ax -O tpgid | grep "single_DAQ_collect" | grep -Eiv "grep" | awk '{print $1}')
+
+if [ "$PID" = "" ]; then
+    echo -e "Unable to find DAQ Driver PID -- is it running?"
+    kill -SIGINT $$
+fi
+
 kill -n 9 $PID
 echo -e "pid:$PID DAQ Driver -- Killed"
