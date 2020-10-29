4483be2212f5dcf51607792b61c742b019c0ee91
caileighf <caileighfitzgerald@gmail.com>
2020-10-29 13:51:39 -0400

    wip

--- scripts/utils/rsync_data.sh
+++ scripts/utils/rsync_data.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 function get_yn() {
-    eval PROMPT="$1"
-    echo "$PROMPT"
     while true; do
         read -p "Please answer [Yes/No]: " yn
         case $yn in
@@ -125,13 +123,15 @@ END
 echo -e "$HEADER\n$PAYLOAD\n" | tee "$TEMP_LOG"
 
 if [ "$CLEAN" = true ]; then
-    RESP=$(get_yn 'Are you sure you want to delete the source files after transfer?')
+    echo "Are you sure you want to delete the source files after transfer?"
+    RESP=$(get_yn)
     if [ "$RESP" = false ]; then
         kill -SIGINT $$
     fi
 fi
 
-RESP=$(get_yn 'Start\ transfer?')
+echo "Start transfer?"
+RESP=$(get_yn)
 if [ "$RESP" = false ]; then
     kill -SIGINT $$
 fi
@@ -149,7 +149,8 @@ else
 fi
 
 # ask if they want log header copied
-RESP=$(get_yn 'Would you like the log file or \"receipt\" for this transaction transferred as well?')
+echo "Would you like the log file or \"receipt\" for this transaction transferred as well?"
+RESP=$(get_yn)
 if [ "$RESP" = true ]; then
     rsync -arPhv "$TEMP_LOG" "$REMOTE_USER@$REMOTE_IP:$REMOTE_DEST" --remove-source-files;
 fi
